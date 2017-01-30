2aaedaa135eeb1be7f9fc7be25f3629b7459586a
student <student@6700k-mac.local>
2017-01-30 20:45:54 +0900

    Change initial light pos (0, 0, 1)

--- Test/Box.c
+++ Test/Box.c
@@ -4,21 +4,18 @@
 
 int main()
 {
-	const int Size = 320;
+	const int s = 320;
 	
-	g_init("Marker", Size*2, Size*2);
+	g_init("Marker", s*2, s*2);
 	
-	g_init_light(0, 0, 0, -1);
+	g_area_color_3D(0.5, 0.75, 0.5, 1);
 	
-//	g_scr_color(1, 1, 1);
-	g_line_color(0, 0, 0, 1);
-	g_line_width(4);
+	g_def_scale_2D(0, -1.5, 1.5, -1.5, 1.5, 0, 0, s, s);
+	g_def_scale_2D(1, -1.5, 1.5, -1.5, 1.5, s, 0, s, s);
 	
-	g_def_scale_2D(0, -1.5, 1.5, -1.5, 1.5, 0, 0, Size, Size);
-	g_def_scale_2D(1, -1.5, 1.5, -1.5, 1.5, Size, 0, Size, Size);
-	
-	g_def_scale_3D(2, -1, 1, -1, 1, -1, 1, 0, Size, Size, Size, 0, 0, 1, 1);
-	
+	g_def_scale_3D(2, -1, 1, -1, 1, -1, 1, 0, s, s, s, 0, 1, 2, 8);
+	g_def_scale_3D(3, -1, 1, -1, 1, -1, 1, s, s, s, s, 0, 1, 2, 8);
+
 	for (double t = 0;; t += 1./128) {
 //		double x = cos(t), y = sin(t);
 		
@@ -31,7 +28,10 @@ int main()
 		g_box_2D_vert(-1, 1, -0.5, 0.5, WIRE_OR_FILL);
 		
 		g_sel_scale_3D(2);
-		g_box_3D(0, 0, 0, 2, 1, 1.5);
+		g_box_3D_core(0, 0, 0, 2, 1, 1.5, 0, WIRE_OR_FILL);
+		
+		g_sel_scale_3D(3);
+		g_box_3D_vert_core(-1, 1, -0.5, 0.5, -0.75, 0.75, 0, WIRE_OR_FILL);
 		
 		g_finish();
 	}
