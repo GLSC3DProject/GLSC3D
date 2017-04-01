acd87e150ae7c94f94f3e15c39a07a6882ebe2a7
Kazuma Tateiri <kazuma.tateiri16@gmail.com>
2017-04-01 16:52:55 +0900

    Removed realloc/malloc branch (realloc acts the same way as malloc if pointer is NULL)

--- Source/g_private.cpp
+++ Source/g_private.cpp
@@ -8,10 +8,7 @@ void *GLSC3D_Array_Buffer(int Array_Size)
 	while (Array_Size > GLSC3D_Data_Buffer_Size)
 		GLSC3D_Data_Buffer_Size *= 2;
 
-	if (GLSC3D_Data_Buffer != NULL)
-		GLSC3D_Data_Buffer = realloc(GLSC3D_Data_Buffer, GLSC3D_Data_Buffer_Size);
-	else
-		GLSC3D_Data_Buffer = malloc(GLSC3D_Data_Buffer_Size);
+	GLSC3D_Data_Buffer = realloc(GLSC3D_Data_Buffer, GLSC3D_Data_Buffer_Size);
 
 	if (GLSC3D_Data_Buffer == NULL) {
 		printf("Memory is ... in GLSC3D_Array_Buffer\n");
