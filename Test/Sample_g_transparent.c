a73dd5b01a6a3f830aebbbc2fa36fc19458a22a4
Kazuma Tateiri <kazuma.tateiri16@gmail.com>
2017-04-04 14:14:16 +0900

    Modified Sample_g_transparent.c

--- Test/Sample_g_transparent.c
+++ Test/Sample_g_transparent.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 #define PI M_PI
-#define G_REAL double
 
 int main()
 {
@@ -12,9 +11,6 @@ int main()
 	double shu = 10;
 	
 	g_init_core("GRAPH", 1280, 960,50,50,0,0,0,G_TRUE, 0, 0);
-
-    FILE *fp;
-	fp = fopen("data.txt","a");
 
 	while(t < shu * 2 * PI)
 	{
@@ -39,8 +35,8 @@ int main()
 		{
 			g_sel_scale_3D(id);
 
+			g_line_width(2);
 			g_area_color_3D(1, 0, 0, 0.4);
-			//g_line_width(2);
 			g_line_color(1,0, 0, 1);
 			g_arrow_3D(-2*scale, 0, 0, 1, 0, 0, 4*scale, 0.1*scale);
 
@@ -52,95 +48,13 @@ int main()
 			g_line_color(0, 0, 1, 1);
 			g_arrow_3D(0, 0, -2*scale, 0, 0, 1, 4*scale, 0.1*scale);
 
-			g_area_color_3D(1, 0, 1, 1);
+			g_area_color_3D(1, 0, 1, 0.4);
 
-			/********************test sphere*******************************/
-			//g_sphere_3D_core(0, 0, 0, scale*1, (int)d_order, 0, G_WIRE);
-
-			/********************test sphere*******************************/
-//			g_cylinder_3D_core(0, 0, 0,
-//					1, 1, 1,
-//					1, 2, 0,
-//					5, 0, G_FALSE, G_FILL);
-
-			/********************test sphere*******************************/
 			g_cone_3D_core(0.5, 1, 0.5,
 			  1, 1, 1,
 			  1, 2, 100, 0, G_FILL);
-
-//			g_arrow_3D_core(0, 0, 0, 1, 0, 1, 2*scale, 0.5*scale, 0, G_WIRE);
-
-			/********************test sphere*******************************/
-//			g_ellipse_3D_core(1, 0.5, 0.5,
-//					1, 1, 1,
-//					1, 1, 1,
-//					6, 0, G_FILL);
-
-			/********************test sphere*******************************/
-//			            g_prism_3D_core(0, 0, 0,
-//			                            1, 1, 1,
-//			                            1, 2, 0, 5,
-//			                            0, G_WIRE, G_FILL);
-
-			/********************test triangle*******************************/
-			//            double length=2;
-			//
-			//            g_triangle_3D_core(length,0,0,
-			//                               0,length,0,
-			//                               0,0,length,
-			//                               2,G_FILL);
-
-			/********************test fan*******************************/
-//			            g_fan_3D_core(1,1,1,
-//			                          -1,1,-1,
-//			                          1,0.5*M_PI,0,
-//			                          5,0,G_FILL);
-
-			/********************test circle*******************************/
-			//            g_circle_3D_core(0,0,0,1,
-			//                             0,0,10,
-			//                             0,0);
-
-			/********************test circle*******************************/
-			/*g_box_3D_core(0,0,0,
-			  1,1,1,
-			  0, G_WIRE);
-			 */
-			/********************test circle*******************************/
-//			g_pyramid_3D_core(0,0,0,
-//			  0,0,1,
-//			  1,1,0,
-//			  5,5,G_FILL);
-
-			/********************test circle*******************************/
-			//            g_rectangle_3D_core(0,0,0,
-			//                                0,1,0,
-			//                                1,2,0,
-			//                                1,0);
-
-			/*******************test bird_view******************************/
-			//            int i_time;
-			//            int Imax = 30,Jmax = 20;
-			//            int i,j;
-			//            double xx,yy,dx=4.0/Imax,dy=4.0/Jmax;
-			//            double array[Imax*Jmax];
-			//            double array2[Imax][Jmax];
-			//            
-			//            for (i = 0; i < Imax; i++)
-			//            {
-			//                xx = dx * i;
-			//                for (j = 0; j < Jmax; j++)
-			//                {
-			//                    yy = dy * j;
-			//                    array[i*Jmax+j] = sin(2*xx*0.1*t - 1) * cos(3*yy*0.1*t - 0.5);
-			//                    array2[i][j] = sin(2*xx*0.1*t - 1) * cos(3*yy*0.1*t - 0.5);
-			//                }
-			//            }
-			//            //g_bird_view_3D(-1, 1, -1, 1, -1, 1, Imax, Jmax, array2, 1);
-			//            g_bird_view_f_3D(-1, 1, -1, 1, -1, 1, Imax, Jmax, array, 1);
 
         }
 		g_finish();
 	}
 }
-
