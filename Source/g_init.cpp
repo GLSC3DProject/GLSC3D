c3b602974855343dc35462156e053c6df4748000
Kazuma Tateiri <kazuma.tateiri16@gmail.com>
2017-04-04 11:17:43 +0900

    Added glPolygonMode

--- Source/g_init.cpp
+++ Source/g_init.cpp
@@ -25,7 +25,7 @@ void g_init_core(
 //	printf("OpenGL Version : %s\n", glGetString(GL_VERSION));
 
 #ifdef G_ENABLE_OPENGL_DEBUG_CALLBACK
-	//glDebugMessageCallback(g_debug_callback, NULL);
+//	glDebugMessageCallback(g_debug_callback, NULL);
 #endif
 
 	glDepthFunc(GL_LEQUAL);
@@ -34,10 +34,7 @@ void g_init_core(
 
 //	glEnable(GL_LINE_SMOOTH);
 //	glEnable(GL_CULL_FACE);
-
-//	int range[2];
-//	glGetIntegerv(GL_POINT_SIZE_RANGE, range);
-//	printf("Supported point size range : [%d, %d]\n", range[0], range[1]);
+//	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	g_vertex_buffer_init();
 
