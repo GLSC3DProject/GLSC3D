ba8d0080df94bcb6034b7abfaa5fbbc11faeceb4
TakamichiSushida <takamichi.sushida@gmail.com>
2017-04-05 19:32:17 +0900

    Modified Sample_g_pyrmid.c

--- Test/Sample_g_pyramid.c
+++ Test/Sample_g_pyramid.c
@@ -1,26 +1,39 @@
-#include<stdlib.h>
 #include<stdio.h>
 
 #include "glsc3d.h"
 
-#define WINDOW_SIZE_X     (500)
+#define WINDOW_SIZE_X     (1000)
 #define WINDOW_SIZE_Y     (500)
 
-int main()
+int main(void)
 {
     g_init("GRAPH", WINDOW_SIZE_X, WINDOW_SIZE_Y);
 
-    g_def_scale_3D(0,-2, 2, -2, 2, -2, 2,0, 0,WINDOW_SIZE_X, WINDOW_SIZE_Y,0, 1, 1, 1);
-    g_init_light(0, 1, -1, 3);
+    g_def_scale_3D(0,
+                   -2, 2, -2, 2, -2, 2,
+                   0, 0, WINDOW_SIZE_X/2, WINDOW_SIZE_Y,
+                   0, 0.0, 1,
+                   1);
+    g_def_scale_3D(1,
+                   -2, 2, -2, 2, -2, 2,
+                   WINDOW_SIZE_X/2, 0, WINDOW_SIZE_X/2, WINDOW_SIZE_Y,
+                   0, 0.0, 1,
+                   1);
 
+    g_init_light(0, 0, 0.0, 1);
+    
     int i_time;
     for(i_time = 0;;i_time++)
     {
         g_cls();
         g_sel_scale_3D(0);
+        g_line_color(1, 0, 0, 1);
+        g_pyramid_3D(0, -2, 0, 0, 1, 0, 2, 4, M_PI/8, 10, G_YES, G_NO);
+
+        g_sel_scale_3D(1);
         g_area_color_3D(1, 0, 0, 1);
-        g_pyramid_3D(0,-2,0,0,1,0,2,4,M_PI/6,6,G_NO,G_YES);
-        
+        g_pyramid_3D_core(0, -2, 0, 0, 1, 0, 2, 4, M_PI/8, 10, 4, G_NO, G_YES);
+
         g_finish();
     }
     return 0;
