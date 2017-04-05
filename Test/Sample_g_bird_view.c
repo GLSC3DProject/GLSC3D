dd0889d5b5d64438600cf4b0f1781ed3b983b63d
TakamichiSushida <takamichi.sushida@gmail.com>
2017-04-05 16:51:27 +0900

    Modified g_bird_view_3D.cpp

--- Test/Sample_g_bird_view.c
+++ Test/Sample_g_bird_view.c
@@ -2,9 +2,6 @@
 
 #include "glsc3d.h"
 
-#define g_bird_view_3D(x_left,x_right,y_bottom,y_top,N_x,N_z, data, wire, fill)\
-(g_bird_view_f_3D((x_left),(x_right),(y_bottom),(y_top),(N_x),(N_z),(double(*)[N_z])(data), wire, fill))
-
 #define INTV              (1)
 #define WINDOW_SIZE_X     (1024)
 #define WINDOW_SIZE_Y     (512)
@@ -24,7 +21,8 @@ int main(void)
 	g_def_scale_3D_core(1, -LEN/2.0, LEN/2.0, -LEN/2.0, LEN/2.0, -LEN/2.0, LEN/2.0, WINDOW_SIZE_X/2, 0, WINDOW_SIZE_X/2, WINDOW_SIZE_Y,1, 1, 1, 0, 0, 0, 1);
 	
 	g_init_light_core(0, 1, 2, 3, 1);
-	
+
+
 	int i_time;
 	double dt = 0.03;
 	int i,j;
@@ -37,13 +35,11 @@ int main(void)
 	{
 		double t = dt*i_time;
 
-		for (j = 0; j < Jmax; j++)
-		{
-			yy = dy * (j + 0.5) - LEN/2.0;
-			for (i = 0; i < Imax; i++)
-			{
-				xx = dx * (i + 0.5) - LEN/2.0;
-				array[j*Imax + i] = sin(2*xx - t) * cos(3*yy - t) ;
+		for (j = 0; j < Jmax; j++) {
+			yy = dy * (j + 0.5) - LEN / 2.0;
+			for (i = 0; i < Imax; i++) {
+				xx = dx * (i + 0.5) - LEN / 2.0;
+				array[j * Imax + i] = sin(2 * xx - t) * cos(3 * yy - t);
 			}
 		}
 		for (i = 0; i < Imax; i++)
@@ -56,18 +52,18 @@ int main(void)
 			}
 		}
 		g_cls();
-		
+
 		g_sel_scale_3D(0);
 		g_area_color_3D(1, 0, 0, 1);
 		g_line_color(1, 0, 0, 1);
 		g_bird_view_3D(-LEN/2.0, LEN/2.0, -LEN/2.0, LEN/2.0, Imax, Jmax, array2, G_TRUE, G_FALSE);
-		
-		
+
 		g_sel_scale_3D(1);
 		g_area_color_3D(1,0,0,1);
 		g_bird_view_f_3D(-LEN/2.0, LEN/2.0, -LEN/2.0, LEN/2.0, Imax, Jmax, array, G_FALSE, G_TRUE);
-		
+
 		g_finish();
     }
+
 	return 0;
 }
