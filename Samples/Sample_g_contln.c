f0a6c2e19dd7469fc0c21a1065ac1b5055d55b55
masakazugit <akiyama@es.hokudai.ac.jp>
2017-04-06 16:42:50 +0900

    akiyama

--- Samples/Sample_g_contln.c
+++ Samples/Sample_g_contln.c
@@ -44,7 +44,9 @@ int main()
 		
 		g_cls();
 		g_sel_scale_2D(0);
+		g_line_color(1.0,0.0,0.0,1.0);
 		g_contln_f_2D(-1.9, 1.9, -1.9, 1.9, Imax, Jmax, array, -0.5);
+		g_line_color(0.0,1.0,0.0,1.0);
 		g_contln_2D(-1.9, 1.9, -1.9, 1.9, Imax, Jmax, array2, 0.5);
 		
 		g_finish();
