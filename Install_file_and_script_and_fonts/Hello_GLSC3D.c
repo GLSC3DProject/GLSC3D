4dc304e681621cca41d9e05b7498e75e4f064c86
Kazuma Tateiri <kazuma.tateiri16@gmail.com>
2017-04-26 02:01:37 +0900

    Indentation

--- Install_file_and_script_and_fonts/Hello_GLSC3D.c
+++ Install_file_and_script_and_fonts/Hello_GLSC3D.c
@@ -7,17 +7,17 @@
 int main()
 {
 	g_init("Hello GLS3D", WINDOW_SIZE_X, WINDOW_SIZE_Y);
-	
-	g_def_scale_3D(0,-2, 2, -2, 2, -2, 2, WINDOW_SIZE_X, 0,WINDOW_SIZE_X, WINDOW_SIZE_Y, 1, 1, 1, 1);
-	
-	for(int i_time = 0;;i_time++)
+
+	g_def_scale_3D(0, -2, 2, -2, 2, -2, 2, WINDOW_SIZE_X, 0, WINDOW_SIZE_X, WINDOW_SIZE_Y, 1, 1, 1, 1);
+
+	for (int i_time = 0;; i_time++)
 	{
 		g_cls();
 		g_sel_scale_2D(0);
-        g_text_size(32);
-        g_text_color(0,0,0,1);
-        g_text_standard(130, 250, "Hello GLSC3D!!");
-        
+		g_text_size(32);
+		g_text_color(0, 0, 0, 1);
+		g_text_standard(130, 250, "Hello GLSC3D!!");
+
 		g_finish();
 	}
 	return 0;
