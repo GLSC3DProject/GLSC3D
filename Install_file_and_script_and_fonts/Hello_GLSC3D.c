a2c9c71ae2a092273edb42939be44bc24e4f5de6
kazuma <kazuma.tateiri16@gmail.com>
2017-07-18 18:08:56 +0900

    Hello_GLSC3D.c

--- Install_file_and_script_and_fonts/Hello_GLSC3D.c
+++ Install_file_and_script_and_fonts/Hello_GLSC3D.c
@@ -8,12 +8,7 @@ int main()
 {
 	g_init("Hello GLS3D", WINDOW_SIZE_X, WINDOW_SIZE_Y);
 
-	g_def_scale_2D(0, -2, 2, -2, 2, -2, 2, WINDOW_SIZE_X, 0, WINDOW_SIZE_X, WINDOW_SIZE_Y);
-//	void g_def_scale_2D(
-//		int id,                                                         //id
-//		double x_left, double x_right, double y_bottom, double y_top,   //仮想座標系
-//		double x_left_std, double y_top_std,                            //ウィンドウの位置
-//		double width_std, double height_std)
+	g_def_scale_2D(0, -2, 2, -2, 2, WINDOW_SIZE_X, 0, WINDOW_SIZE_X, WINDOW_SIZE_Y);
 
 	for (int i_time = 0;; i_time++)
 	{
