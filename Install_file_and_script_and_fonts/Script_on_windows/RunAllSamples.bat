9018577a42c28270099bec302617b778fe132821
kazuma <kazuma.tateiri16@gmail.com>
2017-08-26 15:00:31 +0900

    Modified Script_on_Windows

--- Install_file_and_script_and_fonts/Script_on_windows/RunAllSamples.bat
+++ Install_file_and_script_and_fonts/Script_on_windows/RunAllSamples.bat
@@ -4,7 +4,7 @@ set /P run_sample="Do you want to run the Sample programs (Y/N)? "
 
 if /I %run_sample% == Y (
 	for %%x in (*.c) do (
-		call ccg %%x
+		call ccg.bat %%x
 		call %%~nx.exe
 	)
 )
@@ -14,7 +14,7 @@ set /P run_advanced="Do you want to run the Advanced programs (Y/N)? "
 if /I %run_advanced% == Y (
 	pushd Advanced
 	for %%x in (*.c*) do (
-		call ccg %%x
+		call ccg.bat %%x
 		call %%~nx.exe
 	)
 	popd
--- Install_file_and_script_and_fonts/Script_on_windows/ccg.bat
+++ Install_file_and_script_and_fonts/Script_on_windows/ccg.bat
@@ -8,19 +8,16 @@ if [%*] == [] (
 if not defined GLSC3D_VCVARS_INCLUDED (
 	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
 		rem Visual Studio 2017 Community
-		pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
+		call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 	) else if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat" (
 		rem Visual Studio 2015 Community
-		pushd "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64"
+		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
 	) else (
 		echo 'ccg' requires Visual Studio 2017 Community or 2015 Community. Neither is found.
 		goto :eof
 	)
 
-	call vcvars64.bat
-	popd
-
 	set GLSC3D_VCVARS_INCLUDED=1
 )
 
-cl %* /nologo /O2 /EHsc /MD /I "%~dp0..\Include" /link "%~dp0..\Lib\*" opengl32.lib user32.lib
+cl %* /nologo /O2 /EHsc /MD /utf-8 /I "%~dp0..\Include" /link /NODEFAULTLIB:LIBCMT "%~dp0..\Lib\*" opengl32.lib user32.lib
