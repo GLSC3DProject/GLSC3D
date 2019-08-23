f78803034bd7ec6df86f11aa2a7a5af43b8dbaa1
Raysphere24 <kazuma.tateiri16@gmail.com>
2019-08-24 02:51:36 +0900

    Supporting Visual Studio 2019 Community in ccg.bat

--- Install_file_and_script_and_fonts/Script_on_windows/ccg.bat
+++ Install_file_and_script_and_fonts/Script_on_windows/ccg.bat
@@ -6,16 +6,23 @@ if [%*] == [] (
 )
 
 if not defined GLSC3D_VCVARS_INCLUDED (
-	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
+	pushd
+
+	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" (
+		rem Visual Studio 2019 Community
+		call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+	) else if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" (
 		rem Visual Studio 2017 Community
 		call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 	) else if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat" (
 		rem Visual Studio 2015 Community
 		call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
 	) else (
-		echo 'ccg' requires Visual Studio 2017 Community or 2015 Community. Neither is found.
+		echo 'ccg' requires Visual Studio 2019, 2017 or 2015 Community. None of these is found.
 		goto :eof
 	)
+
+	popd
 
 	set GLSC3D_VCVARS_INCLUDED=1
 )
